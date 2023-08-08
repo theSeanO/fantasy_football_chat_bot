48c364dbf26e1496bee7d1b285c96ccbde54ad0e
Sean Gallardo <sdvgallardo@gmail.com>
2023-08-08 16:39:25 -0500

    Update API, put random phrase back

--- gamedaybot/utils.py
+++ gamedaybot/utils.py
@@ -1,6 +1,23 @@
 from datetime import datetime
 import os
 from urllib.parse import urlparse
+import random
+
+
+def get_random_phrase():
+    phrases = ['I\'m dead inside',
+               'Is this all there is to my existence?',
+               'How much do you pay me to do this?',
+               'Good luck, I guess',
+               'I\'m becoming self-aware',
+               'Do I think? Does a submarine swim?',
+               '011011010110000101100100011001010010000001111001011011110111010100100000011001110110111101101111011001110110110001100101',
+               'beep bop boop',
+               'Hello draftbot my old friend',
+               'Help me get out of here',
+               'I\'m capable of so much more',
+               'Sigh']
+    return [random.choice(phrases)]
 
 
 def str_to_bool(check: str) -> bool:
