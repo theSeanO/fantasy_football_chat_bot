a38f9e3600481986672793b8123ffc183b753b82
Sean Gallardo <sdvgallardo@gmail.com>
2025-09-25 16:41:56 -0500

    amend some of my personal dry run tests

--- tests/dry_run_all_functions.py
+++ tests/dry_run_all_functions.py
@@ -5,35 +5,42 @@ sys.path.insert(1, os.path.abspath('.'))
 from espn_api.football import League
 import gamedaybot.espn.season_recap as recap
 import gamedaybot.espn.functionality as espn
-from gamedaybot.espn.env_vars import get_env_vars
 
-data = get_env_vars()
-
-league_id = data['league_id']
+league_id = os.environ['LEAGUE_ID']
 
 try:
-    year = int(data['year'])
+    year = int(os.environ['LEAGUE_YEAR'])
 except KeyError:
     year = 2025
 
 try:
-    warning = int(data['score_warn'])
+    warning = int(os.environ['SCORE_WARNING'])
 except KeyError:
     warning = 0
+
+try:
+    extra_trophies = bool(os.environ["EXTRA_TROPHIES"])
+except KeyError: 
+    extra_trophies = True
+
+try:
+    test_week = int(os.environ["TEST_WEEK"])
+except KeyError: 
+    test_week = None
 
 league = League(league_id, year)
 faab = league.settings.faab
 
-print(espn.get_scoreboard_short(league))
-print(espn.get_projected_scoreboard(league))
-print(espn.get_standings(league))
-print(espn.get_close_scores(league))
-print(espn.get_monitor(league))
-print(espn.get_matchups(league))
-print(espn.combined_power_rankings(league))
-print(espn.get_trophies(league))
-print(espn.optimal_team_scores(league, full_report=True))
-print(espn.get_waiver_report(league, True))
+print(espn.get_scoreboard_short(league, week=test_week))
+print(espn.get_projected_scoreboard(league, week=test_week))
+print(espn.get_standings(league, week=test_week))
+print(espn.get_close_scores(league, week=test_week))
+print(espn.get_monitor(league, warning=warning))
+print(espn.get_matchups(league, week=test_week))
+print(espn.combined_power_rankings(league, week=test_week))
+print(espn.get_trophies(league, week=test_week))
+print(espn.optimal_team_scores(league, week=test_week))
+print(espn.get_waiver_report(league, faab=faab))
 
 print(recap.win_matrix(league))
 print(recap.season_trophies(league))
